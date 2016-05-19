16258f470aecc537bb47928eb404600b81f17544
Steve Jenson <stevej@buoyant.io>
2016-05-19 13:21:16 -0700

    release script now builds darwin and disambiguates linux and darwin builds

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
-GOOS=linux GOARCH=amd64 go build -o slow_cooker_amd64 github.com/buoyantio/slow_cooker
-GOOS=linux GOARCH=arm   go build -o slow_cooker_arm   github.com/buoyantio/slow_cooker
-GOOS=linux GOARCH=386   go build -o slow_cooker_i386  github.com/buoyantio/slow_cooker
+GOOS=linux GOARCH=amd64  go build -o slow_cooker_linux_amd64 github.com/buoyantio/slow_cooker
+GOOS=linux GOARCH=arm    go build -o slow_cooker_linux_arm   github.com/buoyantio/slow_cooker
+GOOS=linux GOARCH=386    go build -o slow_cooker_linux_i386  github.com/buoyantio/slow_cooker
+GOOS=darwin GOARCH=amd64 go build -o slow_cooker_darwin      github.com/buoyantio/slow_cooker
 echo "releases built:"
 ls slow_cooker*
