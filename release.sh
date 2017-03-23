c371bb9fc945ea5e172aae2f1177c44000044047
Steve Jenson <stevej@fruitless.org>
2017-03-23 09:32:28 -0700

    Revert "Fix “case-insensitive import collision”"

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-GOOS=linux GOARCH=amd64  go build -o slow_cooker_linux_amd64 github.com/BuoyantIO/slow_cooker
-GOOS=linux GOARCH=arm    go build -o slow_cooker_linux_arm   github.com/BuoyantIO/slow_cooker
-GOOS=darwin GOARCH=amd64 go build -o slow_cooker_darwin      github.com/BuoyantIO/slow_cooker
+GOOS=linux GOARCH=amd64  go build -o slow_cooker_linux_amd64 github.com/buoyantio/slow_cooker
+GOOS=linux GOARCH=arm    go build -o slow_cooker_linux_arm   github.com/buoyantio/slow_cooker
+GOOS=darwin GOARCH=amd64 go build -o slow_cooker_darwin      github.com/buoyantio/slow_cooker
 echo "releases built:"
 ls slow_cooker_*
